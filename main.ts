bfae6f160487e14b2b1399ceb20cd8e604b60f4e
maryberrypink <157772443+maryberrypink@users.noreply.github.com>
2024-03-10 13:56:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,29 @@
-tiles.setCurrentTilemap(tilemap`level1`)
-let mySprite = sprites.create(img`
-    . . . . . . 5 . 5 . . . . . . . 
-    . . . . . f 5 5 5 f f . . . . . 
-    . . . . f 1 5 2 5 1 3 f . . . . 
-    . . . f 1 3 3 3 3 3 1 3 f . . . 
-    . . . f 3 3 f f f f 3 1 f . . . 
-    . . . f 3 f f d d f f 3 f . . . 
-    . . f 3 f d f d d f d f 3 f . . 
-    . . f 3 f d 3 d d 3 d f 3 f . . 
-    . . f 3 3 f d d d d f 3 3 f . . 
-    . f 3 3 f 3 f f f f 3 f 3 3 f . 
-    . . f f d 3 5 3 3 5 3 d f f . . 
-    . . f d d f 3 5 5 3 f d d f . . 
-    . . . f f 3 3 3 3 3 3 f f . . . 
-    . . . f 3 3 5 3 3 5 3 3 f . . . 
-    . . . f f f f f f f f f f . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-mySprite.setPosition(7, 106)
+function Create_princess () {
+    mySprite = sprites.create(img`
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f f . . . . . 
+        . . . . f 1 5 2 5 1 3 f . . . . 
+        . . . f 1 3 3 3 3 3 1 3 f . . . 
+        . . . f 3 3 f f f f 3 1 f . . . 
+        . . . f 3 f f d d f f 3 f . . . 
+        . . f 3 f d f d d f d f 3 f . . 
+        . . f 3 f d 3 d d 3 d f 3 f . . 
+        . . f 3 3 f d d d d f 3 3 f . . 
+        . f 3 3 f 3 f f f f 3 f 3 3 f . 
+        . . f f d 3 5 3 3 5 3 d f f . . 
+        . . f d d f 3 5 5 3 f d d f . . 
+        . . . f f 3 3 3 3 3 3 f f . . . 
+        . . . f 3 3 5 3 3 5 3 3 f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    mySprite.setPosition(6, 103)
+    controller.moveSprite(mySprite, 100, 0)
+}
+function Foreground () {
+    tiles.setCurrentTilemap(tilemap`level1`)
+    scene.cameraFollowSprite(mySprite)
+}
+let mySprite: Sprite = null
+Create_princess()
+Foreground()
